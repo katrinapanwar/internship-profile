7199df0056ba7ab8acfbd59b230d9160f01d3858
katrinapanwar <katrina2panwar@gmail.com>
2024-05-24 17:52:25 +0530

    finalCommit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,14 @@ import { Day3Component } from './day3/day3.component';
 import { Day4Component } from './day4/day4.component';
 import { Day5Component } from './day5/day5.component';
 import { PopupComponent } from './popup/popup.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import {MatIconModule} from '@angular/material/icon';
+import { HeaderComponent } from './header/header.component';
+import { SocialsComponent } from './socials/socials.component';
+import { LanguagesComponent } from './languages/languages.component';
+import {MatCardModule} from '@angular/material/card';
+import { FrameworkComponent } from './framework/framework.component';
 
 @NgModule({
   declarations: [
@@ -17,12 +25,21 @@ import { PopupComponent } from './popup/popup.component';
     Day3Component,
     Day4Component,
     Day5Component,
-    PopupComponent
+    PopupComponent,
+    HeaderComponent,
+    SocialsComponent,
+    LanguagesComponent,
+    FrameworkComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatSlideToggleModule,
+    MatIconModule,
+    MatCardModule
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
