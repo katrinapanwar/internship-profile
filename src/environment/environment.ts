9c957147e084899add603b9454e0ac0259bc2941
Entangle Software <entanglesoftware@gmail.com>
2024-08-10 15:33:15 +0530

    Fixed minor issues

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
-    production: false,
-    apiUrl: 'http://ec2-13-233-251-193.ap-south-1.compute.amazonaws.com:8080/'
-  };
-  
+  production: false,
+  apiUrl: 'https://katrinapanwar.entanglesoftware.com/',
+};
